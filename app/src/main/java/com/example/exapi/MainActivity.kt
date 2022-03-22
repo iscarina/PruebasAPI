32355a2b8d4b3cc3b0dd0aa40c82ca9a8e887ba2
iscarina <iscarina@unirioja.es>
2022-03-22 18:57:17 +0100

    no va

--- app/src/main/java/com/example/exapi/MainActivity.kt
+++ app/src/main/java/com/example/exapi/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.exapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -12,8 +13,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        searchByCity("Logroño")
     }
-    // https://api.openweathermap.org/data/2.5/weather?q=London&appid=8ac149135d46fa405913aee0781f1498&units=metric
+    // https://api.openweathermap.org/data/2.5/weather?q=London&appid=8ac149135d46fa405913aee0781f1498&units=metric&lang=es
 
     private fun getRetrofit():Retrofit{
         return Retrofit.Builder()
@@ -24,14 +26,17 @@ class MainActivity : AppCompatActivity() {
 
     private fun searchByCity(ciudad:String){
         CoroutineScope(Dispatchers.IO).launch {
-            val call = getRetrofit().create(APIService::class.java).getWeatherByCity("weather?q=$ciudad&appid=8ac149135d46fa405913aee0781f1498&units=metric")
+            val call = getRetrofit().create(APIService::class.java).getWeatherByCity("weather?q=$ciudad&appid=8ac149135d46fa405913aee0781f1498&units=metric&lang=es")
             val temperatura = call.body()
             if(call.isSuccessful){
-
+                var temp = temperatura?.temperatura
+                val tv: TextView = findViewById(R.id.temperatura)
+                tv.text = temp.toString()
             }else{
 
             }
         }
+
     }
 
 }
---
+++ app/src/main/java/com/example/exapi/WeatherViewHolder.kt
@@ -0,0 +1,13 @@
+package com.example.exapi
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherViewHolder(view : View):RecyclerView.ViewHolder(view) {
+
+    //private val binding = bind(view)
+
+    fun bind(temp:Double){
+        //binding.temperatura
+    }
+}
