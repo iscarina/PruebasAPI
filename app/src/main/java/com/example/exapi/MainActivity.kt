bec6d61b38be859acc673be740c1f6e766afe603
iscarina <iscarina@unirioja.es>
2022-03-29 17:49:37 +0200

    funciona la api porfin

--- app/src/main/java/com/example/exapi/MainActivity.kt
+++ app/src/main/java/com/example/exapi/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.exapi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.example.exapi.data.APIService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val apiService = APIService()
+        GlobalScope.launch(Dispatchers.Main){
+            val currentWeatherResponse = apiService.getCurrentWeather("Logroño").await()
+            val tv : TextView = findViewById(R.id.textView)
+            tv.text = currentWeatherResponse.main.toString()
+        }
     }
 }
